3e8204ef9695d1c2ba3457a22cc582530e9a63d7
Josue Valverde & Yelena Nedelko <student@epicodus.com>
2015-08-04 11:27:27 -0700

    Refactored time_to_sleep code by removing pop and push methods to shorten code by 2 lines.

--- lib/time.rb
+++ lib/time.rb
@@ -2,12 +2,10 @@ class String
   define_method(:time) do
     split_time= split("/")
     new_split_time = []
-    split_time.unshift(split_time[2])
-    split_time.pop()
     split_time.each() do |time|
       new_split_time.push(time.to_i())
     end
-    time = Time.new(new_split_time[0],new_split_time[1],new_split_time[2])
+    time = Time.new(new_split_time[2],new_split_time[1],new_split_time[0])
     if time.saturday?() || time.sunday?()
       "You can sleep in today!"
     else
